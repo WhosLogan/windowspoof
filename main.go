00eceaccce59137a553392d3e6744dfe77d4da02
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-02-18 22:42:02 -0500

    Change window title and remove frame

--- main.go
+++ main.go
@@ -17,17 +17,19 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "windowspoof",
-		Width:  1024,
+		Title:  "Window Spoof",
+		Width:  600,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 24, G: 24, B: 27, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
 		},
+		DisableResize: true,
+		Frameless:     true,
 	})
 
 	if err != nil {
