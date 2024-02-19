0961f58709706a3ee5bde86982163c676a43c598
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-02-19 01:27:29 -0500

    Add go communication functions

--- app.go
+++ app.go
@@ -2,26 +2,32 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+	"time"
 )
 
-// App struct
 type App struct {
 	ctx context.Context
 }
 
-// NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) SetWindowTitle(pid int, title string) {
+
+}
+
+func (a *App) BeginFetchingWindows() {
+	go func() {
+		for {
+			w := GetWindows()
+			runtime.EventsEmit(a.ctx, "window-update", w)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
 }
