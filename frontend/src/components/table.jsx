2d6b69d6bc138b9f917c5ab0a4720c3e9b844dda
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-02-19 01:29:36 -0500

    No select on tables

--- frontend/src/components/table.jsx
+++ frontend/src/components/table.jsx
@@ -69,7 +69,7 @@ export function TableHeader({ className, ...props }) {
       {...props}
       className={clsx(
         className,
-        'border-b border-b-zinc-950/10 px-4 py-2 font-medium first:pl-[var(--gutter,theme(spacing.2))] last:pr-[var(--gutter,theme(spacing.2))] dark:border-b-white/10',
+        'select-none border-b border-b-zinc-950/10 px-4 py-2 font-medium first:pl-[var(--gutter,theme(spacing.2))] last:pr-[var(--gutter,theme(spacing.2))] dark:border-b-white/10',
         grid && 'border-l border-l-zinc-950/5 first:border-l-0 dark:border-l-white/5',
         !bleed && 'sm:first:pl-2 sm:last:pr-2'
       )}
@@ -88,7 +88,7 @@ export function TableCell({ className, children, ...props }) {
       {...props}
       className={clsx(
         className,
-        'relative px-4 first:pl-[var(--gutter,theme(spacing.2))] last:pr-[var(--gutter,theme(spacing.2))]',
+        'select-none relative px-4 first:pl-[var(--gutter,theme(spacing.2))] last:pr-[var(--gutter,theme(spacing.2))]',
         !striped && 'border-b border-zinc-950/5 dark:border-white/5',
         grid && 'border-l border-l-zinc-950/5 first:border-l-0 dark:border-l-white/5',
         dense ? 'py-2.5' : 'py-4',
