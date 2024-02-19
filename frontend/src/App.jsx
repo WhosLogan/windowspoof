75378f18e0cb5f3dc8bcb0591b8c31d7353bec77
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-02-19 01:29:45 -0500

    Finish ui design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,56 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import {Text} from "./components/text.jsx";
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "./components/table.jsx";
+import {Input} from "./components/input.jsx";
+import {Button} from "./components/button.jsx";
+import {BeginFetchingWindows} from "../wailsjs/go/main/App.js";
+import {useEffect, useState} from "react";
+import {EventsOn} from "../wailsjs/runtime/runtime.js";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+    const [windows, setWindows] = useState([]);
 
-    function greet() {
-        Greet(name).then(updateResultText);
+    useEffect(() => {
+        BeginFetchingWindows();
+    }, []);
+
+    EventsOn('window-update', (w) => {
+        setWindows(w)
+    })
+
+    const shortenString = (str) => {
+        return str.length < 15 ? str : `${str.substring(0, 15)}...`
     }
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
+            <div className="flex draggable justify-center pt-2 pb-3">
+                <Text className="select-none">Window Spoof</Text>
+            </div>
+            <div className="mx-8 border-white/15 border rounded-lg px-3 pt-2 mt-2">
+                <Table className="max-h-[37rem]">
+                    <TableHead>
+                        <TableRow>
+                            <TableHeader>Process ID</TableHeader>
+                            <TableHeader>Name</TableHeader>
+                            <TableHeader>Window Title</TableHeader>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {windows.map(w => (
+                            <TableRow key={w.pid}>
+                                <TableCell className="text-zinc-200">{w.pid}</TableCell>
+                                <TableCell className="text-zinc-200">{shortenString(w.name)}</TableCell>
+                                <TableCell className="text-zinc-200">{shortenString(w.windowName)}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+            <div className="fixed bottom-0 w-full">
+                <div className="flex justify-center gap-x-6 items-center p-4 border rounded-lg border-white/15 mb-6 mx-10">
+                    <Input className="max-w-48" placeholder="New Title"/>
+                    <Button className="max-h-10">Change Title</Button>
+                </div>
             </div>
         </div>
     )
